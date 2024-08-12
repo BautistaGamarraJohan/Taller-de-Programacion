03d70aa541d8eb3e4def504def6cf66bd0b4cbdf
gamar <gamar@DESKTOP-3NR67PG>
2024-08-12 15:38:58 -0500

    priemrintento

---
+++ src/main/java/com/mycompany/taller_de_programacion/Taller_de_Programacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller_de_programacion;
+
+/**
+ *
+ * @author gamar
+ */
+public class Taller_de_Programacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
